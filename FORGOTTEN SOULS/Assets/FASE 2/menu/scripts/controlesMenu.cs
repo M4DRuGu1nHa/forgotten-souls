40df0a52b6faed2f430a2fca926a8885345f73ae
M4DRuGu1nHa <machadomatheus760@gmail.com>
2021-08-17 11:10:42 -0300

    Atualizaçao menu 1.0

--- FORGOTTEN SOULS/Assets/FASE 2/menu/scripts/controlesMenu.cs
+++ FORGOTTEN SOULS/Assets/FASE 2/menu/scripts/controlesMenu.cs
@@ -2,21 +2,45 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 
 public class controlesMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Resolution[] resolutions;
+    public TMP_Dropdown ResolutionDropdown;
+
+    private void Start()
     {
-        
+        int CurrentResolutionIndex = 0;
+        resolutions = Screen.resolutions;
+
+        ResolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string Option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(Option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                CurrentResolutionIndex = i;
+            }
+        }
+
+        ResolutionDropdown.AddOptions(options);
+        ResolutionDropdown.value = CurrentResolutionIndex;
+        ResolutionDropdown.RefreshShownValue();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetResolution(int ResolutionIndex)
     {
-        
+        Resolution resolution = resolutions[ResolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
     public void SetFullscreen(bool isFullscreen)
